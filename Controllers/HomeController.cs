f8157e8b62783296500f2c8261d09251e9d70075
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-04-01 19:49:04 +0200

    sending only first incoming closure to view Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,15 +22,18 @@ namespace BridgeMonitor.Controllers
 
         public IActionResult Index()
         {
-            
+            var closures = GetAllBridgesClosureFromApi();
+            var today = DateTime.Now;
 
-            return View();
+            closures.RemoveAll(closure => closure.ReopeningDate < today);
+            return View(closures[0]);
         }
 
         
         public IActionResult AllClosures()
         {
             var closures = GetAllBridgesClosureFromApi();
+            var nextclosure = closures[1];
             return View(closures);
         }
 
@@ -47,19 +50,16 @@ namespace BridgeMonitor.Controllers
        
         private static List<BridgeClosure> GetAllBridgesClosureFromApi()
         {
-            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
+            
             using (var client = new HttpClient())
             {
-                //Interrogation de l'URL censée me retourner les données
+               
                 var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
-                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
                 var stringResult = response.Result.Content.ReadAsStringAsync();
-                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
-                //d'un flux de données vers des objets => Déserialisation
-                //d'objets vers un flux de données => Sérialisation
                 var result = JsonConvert.DeserializeObject<List<BridgeClosure>>(stringResult.Result);
-                Console.WriteLine(result);
-                return result.OrderByDescending(x => x.ClosingDate).Reverse().ToList();
+                var sortedresult = result.OrderBy(closing => closing.ClosingDate).ToList();
+
+                return sortedresult;
                 
 
 
