b108f50d2746bce8199012b1319a97622c54e1f3
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-04-01 11:35:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BridgeMonitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
