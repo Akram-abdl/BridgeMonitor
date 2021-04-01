ac5cbab7e393af0314cf1872ea8a8e787f049cf6
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-04-01 11:53:01 +0200

    Adding API connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using BridgeMonitor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BridgeMonitor.Controllers
@@ -32,6 +34,16 @@ namespace BridgeMonitor.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private static List<BridgeClose> GetAllBridgesCloseApi()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<BridgeClosure>>(stringResult.Result);
+                return result;
+            }
         }
     }
 }
