e8713fabd56b6fd7c41a3bac4291eb45213a8e82
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-04-01 14:46:41 +0200

    Update api connection + return result to view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,16 @@ namespace BridgeMonitor.Controllers
 
         public IActionResult Index()
         {
+            
+
             return View();
+        }
+
+        
+        public IActionResult AllClosures()
+        {
+            var closures = GetAllBridgesClosureFromApi();
+            return View(closures);
         }
 
         public IActionResult Privacy()
@@ -35,14 +44,25 @@ namespace BridgeMonitor.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        private static List<BridgeClose> GetAllBridgesCloseApi()
+       
+        private static List<BridgeClosure> GetAllBridgesClosureFromApi()
         {
+            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
             using (var client = new HttpClient())
             {
+                //Interrogation de l'URL censée me retourner les données
                 var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
                 var stringResult = response.Result.Content.ReadAsStringAsync();
+                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
+                //d'un flux de données vers des objets => Déserialisation
+                //d'objets vers un flux de données => Sérialisation
                 var result = JsonConvert.DeserializeObject<List<BridgeClosure>>(stringResult.Result);
-                return result;
+                Console.WriteLine(result);
+                return result.OrderByDescending(x => x.ClosingDate).Reverse().ToList();
+                
+
+
             }
         }
     }
