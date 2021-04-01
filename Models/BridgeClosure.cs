652204e33f337bf9c5e1dc5f46df94fe526460b7
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-04-01 14:47:07 +0200

    Add class BridgeClosure

---
+++ Models/BridgeClosure.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+namespace BridgeMonitor.Models
+{
+    public class BridgeClosure
+    {
+        [JsonProperty("boat_name")]
+        public string BoatName;
+
+        [JsonProperty("closing_type")]
+        public string ClosingType;
+
+        [JsonProperty("closing_date")]
+        public DateTime ClosingDate;
+
+        [JsonProperty("reopening_date")]
+        public DateTime ReopeningDate;
+    }
+}
